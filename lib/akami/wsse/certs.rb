72e509fc43b4daa025733d0ee184501331de9961
Ville Koivula <ville@manogurgeil.net>
2012-09-28 06:53:15 +0300

    Support for pkcs12

--- lib/akami/wsse/certs.rb
+++ lib/akami/wsse/certs.rb
@@ -13,13 +13,24 @@ module Akami
 
       # Returns an <tt>OpenSSL::X509::Certificate</tt> for the +cert_file+.
       def cert
-        @cert ||= OpenSSL::X509::Certificate.new File.read(cert_file) if cert_file
+        @cert ||= case File.extname(cert_file)
+        when ".p12"
+          OpenSSL::PKCS12.new(File.read(cert_file), private_key_password).certificate
+        else
+          OpenSSL::X509::Certificate.new File.read(cert_file)
+        end if cert_file
       end
 
       # Returns an <tt>OpenSSL::PKey::RSA</tt> for the +private_key_file+.
       def private_key
-        @private_key ||= OpenSSL::PKey::RSA.new(File.read(private_key_file), private_key_password) if private_key_file
+        @private_key ||= case File.extname(private_key_file)
+        when ".p12"
+          OpenSSL::PKCS12.new(File.read(private_key_file), private_key_password).key
+        else
+          OpenSSL::PKey::RSA.new(File.read(private_key_file), private_key_password)
+        end if private_key_file
       end
+
     end
   end
 end
