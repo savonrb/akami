728ff79cd324ea4a09b43880d424de143c3c5f05
Jacob Kjeldahl <jacob@kjeldahl.it>
2024-12-16 13:49:02 +0100

    Adds handling of InclusiveNamespaces configuration
    Both for CanonicalizationMethod and Transform (only one)

--- lib/akami/c14n_helper.rb
+++ lib/akami/c14n_helper.rb
@@ -1,8 +1,8 @@
 module Akami
   module C14nHelper
-    def canonicalize(xml)
+    def canonicalize(xml, inclusive_namespaces = nil)
       return unless xml
-      xml.canonicalize Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0
+      xml.canonicalize Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0, inclusive_namespaces
     end
   end
 end
