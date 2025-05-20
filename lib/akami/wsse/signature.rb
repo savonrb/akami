d8eb8f85720ad2260f959feb0dc189644b09ddd0
Jacob Kjeldahl <jacob@kjeldahl.it>
2025-05-20 09:03:10 +0200

    Adds code suggestions from the original PR review

--- lib/akami/wsse/signature.rb
+++ lib/akami/wsse/signature.rb
@@ -196,8 +196,9 @@ module Akami
 
       def timestamp_digest
         return nil unless timestamp
-        timestamp = canonicalize(at_xpath(@document, "//Envelope/Header/Security/Timestamp"))
-        Base64.encode64(OpenSSL::Digest::SHA1.digest(timestamp)).strip
+
+        canonical_timestamp = canonicalize(at_xpath(@document, "//Envelope/Header/Security/Timestamp"))
+        Base64.encode64(OpenSSL::Digest::SHA1.digest(canonical_timestamp)).strip if canonical_timestamp
       end
 
       def signed_info_digest_method
